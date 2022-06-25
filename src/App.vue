5f9be88d27dcabd1184cc76baf94c120bc53b5a2
stefandb <stefan@stefandebruin.eu>
2022-06-25 22:30:36 +0200

    Start with navigation

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
 <template>
-  <div>HALLO</div>
+  <div
+    class="
+      w-full
+      min-h-screen
+      bg-white
+      text-black
+      dark:bg-black dark:text-white
+    "
+  >
+    <nav class="w-full p-8 flex justify-center md:space-x-4">
+      <!-- Start left menu -->
+      <div class="hidden">LEFT</div>
+      <!-- End left menu -->
+
+      <!-- Start LOGO -->
+      <div class="font-Rampart text-3xl md:text-4xl">Stefan de bruin</div>
+      <!-- End LOGO -->
+
+      <!-- Start right menu -->
+      <div class="hidden">RIGHT</div>
+      <!-- End right menu -->
+    </nav>
+  </div>
 </template>
