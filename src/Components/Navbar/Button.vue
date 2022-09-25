f464f9e9386a74ae973fe68be9feb9b0219db302
stefandb <stefan@stefandebruin.eu>
2022-09-25 15:59:25 +0200

    install heroicons

---
+++ src/Components/Navbar/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="
+      px-2
+      py-2
+      rounded-lg
+      flex
+      items-center
+      justify-center
+      shadow-xl
+      border-2
+      text-black
+      dark:text-white
+    "
+  >
+    Projects
+  </div>
+</template>
